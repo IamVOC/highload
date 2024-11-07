c3db1a1693c8452e773d9b2ea6f3b225a4649b8d
Daniil Bozhko <voc.offacc@gmail.com>
2024-11-07 17:57:11 +0600

    fix: change IoC dependency name

--- SpaceBattle.Lib/Strategies/SagaCommandStrategy.cs
+++ SpaceBattle.Lib/Strategies/SagaCommandStrategy.cs
@@ -6,15 +6,15 @@ public class CreateSagaCommand : IStrategy {
 
 	public object run_strategy(params object[] args)
     {
-		List<Tuple<ICommand, ICommand>> l = new List<Tuple<ICommand, ICommand>>();
+		List<Tuple<SpaceBattle.Lib.ICommand, ICommand>> l = new List<Tuple<ICommand, ICommand>>();
 		
-		IEnumerable<string> cmdNames = (IEnumerable<string>)args.Take(args.Length - 1);
+		var cmdNames = args.Take(args.Length - 1);
 		IUObject uobj = (IUObject)args.Last();
 
 		foreach (string cmdName in cmdNames) {
 			var adaptee = IoC.Resolve<object>("Game.Adapter.AdaptForCmd", uobj, cmdName);
 			ICommand cmd = IoC.Resolve<ICommand>("Game.Commands.CreateCommand", cmdName, adaptee);
-			ICommand compensCmd = IoC.Resolve<ICommand>("Game.Saga.GetCompensatingCommand", cmdName, adaptee);
+			ICommand compensCmd = IoC.Resolve<ICommand>("Game.Saga.CreateCompensatingCommand", cmdName, adaptee);
 			l.Add(new Tuple<ICommand, ICommand>(cmd, compensCmd));
 		}
 
